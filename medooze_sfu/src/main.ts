74e622225abfcb4d05e3f3ddcf79111b859f9c30
tkmn0 <tkmgin0914@gmail.com>
2019-08-25 19:57:02 +0900

    docs: update gitignore

--- medooze_sfu/src/main.ts
+++ medooze_sfu/src/main.ts
@@ -2,10 +2,12 @@
 import { ISignalingServer } from "./signaling/signalingServerInterface";
 import { WebSocketServer } from "./signaling/websocketServer";
 import { Sfu } from "./sfu/sfu";
+import { CONFIG } from "./config/config";
 
 class Main {
 
     constructor() {
+        console.log(CONFIG);
         let signalingServer: ISignalingServer = new WebSocketServer();
         let sfuServer = new Sfu(signalingServer);
     }
